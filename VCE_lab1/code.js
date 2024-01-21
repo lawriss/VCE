2f4a1aa2eb15568b5c77a460aa215b3e55a36c4c
lawriss <laura.ruiz17@estudiant.upf.edu>
2024-01-21 21:49:02 +0100

    chat actualizado

--- VCE_lab1/code.js
+++ VCE_lab1/code.js
@@ -11,112 +11,231 @@ elem.style.display = ""; //reapear*/
 ///var message_sent = document.querySelector(".message-sent")
 ///var cloned = message_sent.cloneNode( true ); //returns a copy of this element
 
-//capture user actions
-var chat = document.querySelector(".scroll");
-var input = document.querySelector(".typing-area input");
-input.focus();
 
-function addMessage(str){
-   //create element for the DOM
+// SENDING MESSAGES FUNCTIONS
+
+function addMessage(msg){
+   var chat = document.querySelector(".scroll");
    var elem = document.createElement("div");
    elem.className = "message-sent";
-   elem.innerHTML = str;
+   elem.innerHTML = msg.content;
    chat.appendChild(elem);
    chat.scrollTop = 10000;
+
+   // store message in database
+   msg_history.content.push( msg );
+
+   // Sockets only allow to send strings, so any object must be converted to string before being sent:
+   chat_server.sendMessage(JSON.stringify(msg));
+   //chat_server.storeData("mykey", "mydata");
 }
 
 function onKeyPress(event){
-   if(event.code == "Enter" && user_screen.scree_two==true){
-      addMessage(input.value);
-      input.value = "";
+   if(event.code == "Enter" ){ //&& user_screen.scree_two==true
+      send();
    }
 }
 
 function send(){
-   addMessage(input.value);
+   var msg = {
+      type: "text",
+      content: input.value, //string var
+      id: user.id, //user var
+      name: user.username
+   };
+   addMessage(msg);
    input.value = "";
 }
 
-input.addEventListener("keydown", onKeyPress);
+/*function send(){
+   addMessage(input.value);
+   input.value = "";
+}*/
 
-var send_button = document.querySelector(".send-message");
-send_button.addEventListener("click", send);
 
+// LOG IN WEB
 
-var user = {
-   username: null,
-   room_name: null
-};
-
-var user_screen = {
-   screen_one: true,
-   scree_two: false
-};
-
-function accept(){
-   user.username = document.querySelector(".log-in-username").value;
-   user.room_name = document.querySelector(".log-in-room").value;
-   console.log(user.username);
-   console.log(user.room_name);
-   //////
+function display_chat(){
+   // We hide intro-container and display main-container
    var intro = document.querySelector(".intro-container");
    intro.style.display = "none";
    var main = document.querySelector(".main-container");
-   main.style.display = ""; //reapear
-   /////
-   var aaa = document.querySelector(".profile-bar p");
-   aaa.innerHTML = user.username;
-   ////
-   user_screen.screen_one = false;
-   user_screen.scree_two = true;
+   main.style.display = ""; 
 }
 
-var send_button = document.querySelector(".button-go");
-send_button.addEventListener("click", accept);
-
-
-// We define a protocol
-var msg = {
-	type: "text",
-	content: "hello",
-	username: "Javi"
-};
-
-
-
-
-
-// Sockets only allow to send strings, 
-// so any object must be converted to string before being sent:
-//var msg_str = JSON.stringify( msg );
-
-// And when a message is received from the server, it must be transformed back to object:
-//var msg = JSON.parse( msg_str );
-
-//Try to reuse functions, so you do not have to code a different function to show a message from the users than to show a message from another user.
-//Keep the important data stored in some global vars: data like the users, past messages, etc (!!!!!)
-//You can use several JS files if you want, or Classes.
-//Do not use the DOM to store your data, use global variables. The DOM is only to show info to the user.
-
-//global container to store important stuff
-var DB = {
-	msgs: []
-};
-
-function onMessage(id,msg){
-	//store message
-	DB.msgs.push( msg );
-	displayMessage( msg );
-}
-
-/////////
-/*
-var server = new SillyClient();
-server.connect( "protocol://host_address:port", "room_name");
-server.on_ready = function( my_id )
+function accept()
 {
-	//you know now which is your id
+   user.username = document.querySelector(".log-in-username").value;
+   user.room_name = document.querySelector(".log-in-room").value;
+   console.log("username: "+user.username);
+   console.log("room_name: "+user.room_name);
+   display_chat();
+
+   // Change username of profile-bar
+   var username_profile = document.querySelector(".profile-bar p");
+   username_profile.innerHTML = user.username;
+
+   // Change user-screen (???)
+   //user_screen.screen_one = false;
+   //user_screen.scree_two = true;
+
+   ////////////////
+   chat_server = new SillyClient();
+   // 4. assign url and roomname to the server
+   chat_server.connect("wss://ecv-etic.upf.edu/node/9000/ws", user.room_name);
+
+   // 5. assign id to user
+   chat_server.on_ready = function(my_id)
+   {
+      user.id = my_id;
+      console.log("You are connected, your id is: " + my_id);
+      /*
+      priv_msg = {
+         type: "private",
+         name: user.username
+      } 
+      chat_server.sendMessage(priv_msg)*/
+   }
+
+   // on_message: to get message sent by other users
+   chat_server.on_message = function(author_id, msg)
+   {
+      // When a message is received from the server, it must be transformed back to object:
+      var msg_received = JSON.parse( msg );
+      if (msg_received.type=="text"){
+         load_user_message(msg_received);
+         // store data
+         msg_history.content.push( msg_received );
+      }else if (msg_received.type=="history" && h==false){
+         h=true;
+         for (var i = 0; i <= msg_received.content.length; i++) {
+            load_user_message(msg_received.content[i]);
+            // store data
+            msg_history.content.push( msg_received.content[i] );
+        }
+        
+      }
+      
+   }
+
+   // on_user_connected: to know when new users enter the room
+   chat_server.on_user_connected = function(user_id)
+   {
+      // add new chat to the list of users area
+      var array_chats = document.querySelector(".users-area");
+      var user_chat = document.querySelector(".user-chat")
+      var cloned = user_chat.cloneNode( true );
+      info_user = cloned.querySelector('p');
+      info_user.textContent = "username_"+user_id; //posar username, no id (ns com)
+      array_chats.appendChild(cloned);
+      array_chats.scrollTop = 10000;
+
+      // create and send history var message
+      
+      chat_server.sendMessage(JSON.stringify(msg_history), user_id);
+   }
+
+}
+
+function load_user_message(message){
+   console.log(message);
+   if (message.type=="text"){
+      var elem1 = document.createElement('div');
+      elem1.className = 'message-received';
+      var elem2 = document.createElement('p');
+      elem2.className = 'name';
+      elem2.textContent = '~ '+message.name;
+      var elem3 = document.createElement('p');
+      elem3.textContent = message.content;
+      elem1.appendChild(elem2);
+      elem1.appendChild(elem3);
+      var scroll_div = document.querySelector('.scroll');
+      scroll_div.appendChild(elem1);
+      scroll_div.scrollTop = 10000;
+   }
+   
+}
+// SERVER CALLBACKS
+/*
+
+// on_user_connected: to know when new users enter the room
+server.on_user_connected = function(user_id)
+{
+	// add new chat to the list of users area
+	var array_chats = document.querySelector(".users-area");
+	var elem = document.createElement("div");
+	elem.className = ".user-chat";
+	elem.id = user_id;
+	elem.innerHTML = "username_"+user_id;
+	array_chats.appendChild(elem);
+	array_chats.scrollTop = 10000;
+
+	// create new historial of messages
+	// ...
+}
+
+// on_user_disconnected: to know when users leave the room
+server.on_user_disconnected = function(user_id)
+{
+	// remove user chat to the list of users area
+	var user_discon = document.querySelector("#"+user_id);
+	user_discon.style.display = "none"; //disapear
+
+	// remove historial ...
 }
 */
 
-/*var room_name = "MYSUPERAPP_" + room;*/
+/////// MAIN
+
+/*var user_screen = {
+   screen_one: true,
+   scree_two: false
+};*/
+
+
+// 1. create user and empty dabatase
+var user = {
+   username: null,
+   room_name: null,
+   id: null
+};
+
+var h = false;
+
+var msg_history = {
+   type: "history",
+   content: []
+};
+
+// 2. create server
+//chat_server = new SillyClient();
+
+// 3. User types username and room_name in log in. Then press "Go" button
+var send_button = document.querySelector(".button-go");
+send_button.addEventListener("click", accept);
+
+/*// 4. assign url and roomname to the server
+chat_server.connect("wss://ecv-etic.upf.edu/node/9000/ws", user.room_name);
+
+// 5. assign id to user
+chat_server.on_ready = function(my_id)
+{
+	user.id = my_id;
+   console.log("You are connected, your id is: " + user_id); 
+}*/
+
+// 6. user sends a message
+var msg = {
+	type: "text",
+	content: null, //string var
+	id: null, //user var
+   name: null
+};
+
+
+
+var input = document.querySelector(".typing-area input");
+input.focus();
+var send_msg = document.querySelector(".send-message");
+send_msg.addEventListener("click", send);
+input.addEventListener("keydown", onKeyPress);
--- VCE_lab1/connect_server.js
+++ VCE_lab1/connect_server.js
@@ -2,24 +2,52 @@
 var server = new SillyClient();
 
 //connect to the SillyServer course server installed at the UPF
-server.connect( "wss://ecv-etic.upf.edu/node/9000/ws", "CHAT");
-
-server.on_ready = function( my_id )
-{
-	//you know now which is your id
-}
-
-var room_name = "LAURA_APP_" + room;
+server.connect( "wss://ecv-etic.upf.edu/node/9000/ws", "LAURA_APP_" + user.room_name);
 
 //Also you can have several sillyclient instances if you want to connect to several channels at the same time.
 
-//SillyClient callbacks. I recommend to have callbacks for:
-/*
-on_ready: to know which is your user_id
-on_user_connected: to know when new users enter the room
-on_user_disconnected: to know when users leave the room
-on_message: to get message sent by other users
-on_close: to know if the connection is lost and try to reconnect.*/
+// on_ready: to know which is your user_id
+server.on_ready = function( my_id )
+{
+	user.id = my_id;
+}
+
+// on_user_connected: to know when new users enter the room
+server.on_user_connected = function(user_id)
+{
+	// add new chat to the list of users area
+	var array_chats = document.querySelector(".users-area");
+	var elem = document.createElement("div");
+	elem.className = ".user-chat";
+	elem.id = user_id;
+	elem.innerHTML = "username_"+user_id;
+	array_chats.appendChild(elem);
+	array_chats.scrollTop = 10000;
+
+	// create new historial of messages
+	// ...
+}
+
+// on_user_disconnected: to know when users leave the room
+server.on_user_disconnected = function(user_id)
+{
+	// remove user chat to the list of users area
+	var user_discon = document.querySelector("#"+user_id);
+	user_discon.style.display = "none"; //disapear
+
+	// remove historial ...
+}
+
+// on_message: to get message sent by other users
+server.on_message = function(author_id, msg)
+{
+
+}
+
+// on_close: to know if the connection is lost and try to reconnect.
+server.on_close = function(){
+
+}
 
 
 // RECEIVE MESSAGES
@@ -40,6 +68,7 @@ But remember, it is way better to send objects in string format so you can add f
 Also, you can send private message to one (or several) users if you know its ID:
 server.sendMessage( "my_message", [user1_id, user2_id] );
  */
+server.sendMessage("my_message");
 
 /* PREV MESSAGES
 One important feature is that users can see the previous chat when joining an existing channel. 
@@ -67,3 +96,72 @@ var msg = {
 }
 ]
 };
+
+
+
+//Try to reuse functions, so you do not have to code a different function to show a message from the users than to show a message from another user.
+//Keep the important data stored in some global vars: data like the users, past messages, etc (!!!!!)
+//You can use several JS files if you want, or Classes.
+//Do not use the DOM to store your data, use global variables. The DOM is only to show info to the user.
+
+
+/*var ws = new WebSocket("wss://ecv-etic.upf.edu/node/9000/ws");
+ws.name = "CONNECTION1"
+ws.onmessage = function(msg){
+   console.log(this.name, msg);
+}
+
+var ws2 = new WebSocket("wss://ecv-etic.upf.edu/node/9000/ws");
+ws2.name = "CONNECTION2"
+ws2.onmessage = function(msg){
+   console.log(this.name, msg);
+}*/
+
+var MYCHAT = {
+	//root: null,
+	server: null,
+	//onNewMessage: null,
+	connect: function(url, room_chosen){
+	   MYCHAT.server = new SillyClient();
+	   MYCHAT.server.connect(url, room_chosen);
+	   MYCHAT.server.on_message = function(id, msg){
+		  console.log(id, msg);
+		  MYCHAT.showMessage(id, msg);
+ 
+		  if(MYCHAT.onNewMessage){
+			 MYCHAT.onNewMessage(id,msg);
+		  }
+	   }
+	},
+	createHTML: function(container){
+ 
+	},
+	showMessage: function(author, msg){
+	   var elem = document.createElement("div");
+	   elem.innerText = msg;
+	   this.root.querySelector(".msgs").appendChild(elem);
+	},
+	create: function(container){
+	   var elem = document.createElement("div");
+	   elem.innerHTML = `
+	   <div class="msgs"></div>
+	   <input>`;
+	   container.appendChild(elem);
+	   //this.root = elem;
+	   var input = elem.querySelector("input");
+	   input.addEventListener("keydown", function(e){
+		  if(e.code == "enter"){
+			 MYCHAT.showMessage("unknown", input.value);
+			 MYCHAT.server.sendMessage(input.value);
+			 input.value = "";
+		  }
+	   })
+	}
+ }
+ 
+ 
+ MYCHAT.createHTML(document.querySelector(".main-container"));
+ MYCHAT.connect("wss://ecv-etic.upf.edu/node/9000/ws");
+ MYCHAT.onNewMessage = function(id, msg){
+	console.log("PLAY SOUND!");
+ }
